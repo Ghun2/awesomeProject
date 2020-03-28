1013d895de4b2f5973d0a70feb38c2487376eaa2
ghun <ghun2ee@gmail.com>
2020-03-28 19:54:52 +0900

    chapter ~ 9

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,9 @@ RUN apk update && \
   apk add git && \
   go get github.com/cespare/reflex
 
+# TimeZone 설정
+ENV TZ Asia/Seoul
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 EXPOSE 7777
 CMD ["reflex", "-c", "reflex.conf"]
--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/gommon/log"
-	"net/http"
 	"io/ioutil"
-	"encoding/json"
+	"net/http"
+	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Cat struct {
 	Name string `json:"name"`
 	Type string `json:"type"`
 }
+
+type Dog struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Hamster struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type JwtClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
 
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hel412lo, Wo123rld!")
@@ -74,19 +94,197 @@ func addCat(c echo.Context) error {
 }
 
 
+func addDog(c echo.Context) error {
+	dog := Dog{}
+
+	defer c.Request().Body.Close()
+
+	err := json.NewDecoder(c.Request().Body).Decode(&dog)
+	if err != nil {
+		log.Printf("Failed processing addDog request: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("this is your dog: %#v", dog)
+	return c.String(http.StatusOK, "we got your dog!")
+}
+
+func addHamster(c echo.Context) error {
+	hamster := Hamster{}
+
+	err := c.Bind(&hamster)
+	if err != nil {
+		log.Printf("Failed processing addHamster request: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("this is your hamster: %#v", hamster)
+	return c.String(http.StatusOK, "we got your hamster!")
+}
+
+
+func mainAdmin(c echo.Context) error {
+	return c.String(http.StatusOK, "ji hun you are on the secret admin main page!")
+}
+
+func mainCookie(c echo.Context) error {
+	return c.String(http.StatusOK, "you are on the secret cookie page!")
+}
+
+func mainJwt(c echo.Context) error {
+	user := c.Get("user")
+	token := user.(*jwt.Token)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	log.Print("User Name: ", claims["name"], " User ID: ", claims["jti"])
+
+	return c.String(http.StatusOK, "you are on the top secret jwt page!")
+}
+
+func login(c echo.Context) error {
+	username := c.QueryParam("username")
+	password := c.QueryParam("password")
+
+	// check username and password against DB after hashing the password
+	if username == "yelim" && password == "0526" {
+		// this is the same
+		//cookie := new(http.Cookie)
+		cookie := &http.Cookie{}
+
+		cookie.Name = "sessionID"
+		cookie.Value = "some_string"
+		cookie.Expires = time.Now().Add(48 * time.Hour)
+
+		c.SetCookie(cookie)
+
+		// create jwt token
+		token, err := createJwtToken()
+		if err != nil {
+			log.Print("Error Creating JWT token", err)
+			return c.String(http.StatusInternalServerError, "someting went wrong")
+		}
+
+		// this is the same
+		//cookie := new(http.Cookie)
+		jwtCookie := &http.Cookie{}
+
+		jwtCookie.Name = "JWTCookie"
+		jwtCookie.Value = token
+		jwtCookie.Expires = time.Now().Add(48 * time.Hour)
+
+		c.SetCookie(jwtCookie)
+
+
+		return c.JSON(http.StatusOK, map[string] string {
+			"message": "You were logged in!",
+			"token": token,
+		})
+	}
+	return c.String(http.StatusUnauthorized, "You username or password were wrong")
+}
+
+func createJwtToken() (string, error) {
+	claims := JwtClaims{
+		"yelim",
+		jwt.StandardClaims{
+			Id:        "main_user_id",
+			ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+		},
+	}
+
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	token, err := rawToken.SignedString([]byte("mySecret"))
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+///////////////////////////////////// middlewares ////////////////////////////////////
+
+////////// 커스텀 서버 헤더 ///////////
+func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderServer, "BlueBot/1.0")
+		c.Response().Header().Set("notReallyHeader", "thisHaveNoMearning")
+		return next(c)
+	}
+}
+
+func checkCookie(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie("sessionID")
+		if err != nil {
+			if strings.Contains(err.Error(), "named cookie not present") {
+				return c.String(http.StatusUnauthorized, "you dont have any cookie")
+			}
+			log.Print(err)
+			return err
+		}
+		if cookie.Value == "some_string" {
+			return next(c)
+		}
+
+		return c.String(http.StatusUnauthorized, "you dont have the right cookie, cookie")
+	}
+}
+
 func main() {
 	fmt.Println("Hello ji hun")
+	fmt.Println(time.Now())
 
 	e := echo.New()
+	e.Use(ServerHeader)
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format:"[${time_rfc3339}]  ${status}  ${method}  ${host}${path} ${latency_human}" + "\n",
+	}))
 
-	e.GET("/", hello)
-	e.GET("/cats/:data", getCats)
+	adminGroup := e.Group("/admin")
+	anmalGroup := e.Group("/animal")
 
-	e.GET("/cats", func (c echo.Context) error {
+	cookieGroup := e.Group("/cookie")
+	jwtGroup := e.Group("/jwt")
+
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root: "static",
+		HTML5: true,
+	}))
+
+	adminGroup.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		// check in the DB
+		if username == "hun" && password == "3133" {
+			return true, nil;
+		}
+		return false, nil;
+	}))
+
+	jwtGroup.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningMethod: "HS256",
+		SigningKey: []byte("mySecret"),
+		TokenLookup: "cookie:JWTCookie",
+	}))
+
+	cookieGroup.Use(checkCookie)
+
+	cookieGroup.GET("/main", mainCookie)
+	adminGroup.GET("/main", mainAdmin)
+	jwtGroup.GET("/main", mainJwt)
+
+	e.GET("/hello", hello)
+	e.GET("/login", login)
+
+	anmalGroup.GET("/cats/:data", getCats)
+
+	anmalGroup.GET("/cats", func (c echo.Context) error {
 		return c.String(http.StatusOK, "Hel412lo, Wo123rld!")
 	})
 
-	e.POST("/cats", addCat)
+	anmalGroup.POST("/cats", addCat)
+	anmalGroup.POST("/dogs", addDog)
+	anmalGroup.POST("/hamsters", addHamster)
 
 	e.Logger.Fatal(e.Start(":7777"))
 }
